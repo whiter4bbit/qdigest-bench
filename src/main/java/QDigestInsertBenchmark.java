d226478f6beb907d53105336002ba7d0c5d42643
whiter4bbit <wr4bbit@gmail.com>
2013-08-04 05:03:57 +0300

    Update QDigestInsertBenchmark.java

--- src/main/java/QDigestInsertBenchmark.java
+++ src/main/java/QDigestInsertBenchmark.java
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) Sematext International
- * All Rights Reserved
- * <p/>
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Sematext International
- * The copyright notice above does not evidence any
- * actual or intended publication of such source code.
- */
-
 import com.clearspring.analytics.stream.quantile.QDigest;
 
 import java.util.concurrent.ThreadLocalRandom;
